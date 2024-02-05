d373824f21a62dbc9704fba122f968ebd5d0518f
weiß Yi <78130010+Rosa-Eau@users.noreply.github.com>
2024-02-05 16:00:31 +0900

    20240129

--- Project/app.py
+++ Project/app.py
@@ -3,36 +3,96 @@
 0. Flask : 웹서버를 시작할 수 있는 기능. app이라는 이름으로 플라스크를 시작한다
 1. render_template : html파일을 가져와서 보여준다
 '''
-from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
+# DB 기본 코드
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(10000), nullable=False)
+
+    def __repr__(self):
+        return f'{self.username} {self.title} 추천 by {self.username}'
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
-    
-    name = "오진선"
-    motto = "웃으면 복이 와요"
-    
+    name = '오진선'
+    motto = "Love all, trust a few, Do wrong to none"
+
     context = {
-        'name' : name,
-        'motto' : motto,
+        "name": name,
+        "motto": motto,
     }
-    
     return render_template('motto.html', data=context)
 
-@app.route("/music")
+
+@app.route("/music/")
 def music():
-    return render_template('music.html')
+    song_list = Song.query.all()
+
+    return render_template('music.html', data=song_list)
+
 
 @app.route("/iloveyou/<name>/")
 def iloveyou(name):
-    motto = f"{name}아, 난 너뿐이야!"
-    
+    motto = f"{name}야 난 너뿐이야!"
+
     context = {
-        'name' : name,
-        'motto' : motto,
+        'name': name,
+        'motto': motto,
     }
-    
     return render_template('motto.html', data=context)
+
+
+@app.route("/music/<username>/")
+def render_music_filter(username):
+    filter_list = Song.query.filter_by(username=username).all()
+
+    return render_template('music.html', data=filter_list)
+
+
+@app.route("/music/create/")
+def music_create():
+    # form에서 데이터 받아오기
+    username_receive = request.args.get("username")
+    title_receive = request.args.get("title")
+    artist_receive = request.args.get("artist")
+    image_receive = request.args.get("image_url")
+
+    # 데이터를 DB에 저장하기
+    song = Song(username=username_receive, title=title_receive,
+                artist=artist_receive, image_url=image_receive)
+    db.session.add(song)
+    db.session.commit()
+
+    return redirect(url_for('render_music_filter', username=username_receive))
+
+
+@app.route("/music/delete/<int:id>")
+def music_delete(id):
+    delete_id = Song.query.filter_by(id=id).first()
+    db.session.delete(delete_id)
+    db.session.commit()
+
+    return redirect(url_for('music'))
 
 if __name__ == "__main__":
     app.run(debug=True)
